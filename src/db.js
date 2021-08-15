18f82c77beb957d9c23dc16ce09fed82a08a432c
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2021-08-15 22:18:52 +0500

    Update-10.0

--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+const path = require("path");
+const envpath = path.join(__dirname, "/config.env");
+dotenv.config({ path: envpath })
 
-
-
+const DB = process.env.DATABASE;
+// const DB = "mongodb+srv://azeem:2765@cluster0.jpazi.mongodb.net/sticky_notes101?retryWrites=true&w=majority";
 // mongoose.connect("mongodb://localhost:27017/Sticky_Notes", {
-mongoose.connect("mongodb+srv://azeem:2765@cluster0.jpazi.mongodb.net/sticky_notes101?retryWrites=true&w=majority", {
+mongoose.connect(`${DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
